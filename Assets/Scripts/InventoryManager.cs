ede5fe4d7c6c4e84f667fbe63dd9613670e0682c
VitaL45 <dheerajpatel345@outlook.com>
2025-07-23 21:26:48 +0530

    content added

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject InventoryMenu;
+    private bool menuActivated;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory") && menuActivated)
+        {
+            Time.timeScale = 1;
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        else if (Input.GetButtonDown("Inventory") && !menuActivated)
+        {
+            Time.timeScale = 0;
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        Debug.Log("itemName = " + itemName + ", quantity = " + quantity + ", itemSprite = " + itemSprite);
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private float moveSpeed = 1f;
+    Vector2 movement = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement = Vector2.right;
+        }
+        rb.velocity = movement * moveSpeed;
+    }
+}
